210b0042ae9c8e51e2acde57227bf5b2ca6490d3
@cawfree <cawfree@gmail.com>
2019-07-02 08:25:18 +0100

    Exported withLayoutAnimation.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import LayoutAnimationProvider from './example/LayoutAnimationProvider';
+import LayoutAnimationProvider, {
+  withLayoutAnimation as withLayoutAnimationConsumer,
+} from './example/LayoutAnimationProvider';
+
+export const withLayoutAnimation = withLayoutAnimationConsumer;
 
 export default LayoutAnimationProvider;
